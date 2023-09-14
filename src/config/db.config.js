cda80f99afe7fe1eae262a189b1aa2056eefc7bb
Dong Ngo <dongngo.2000@gmail.com>
2023-09-14 23:50:54 +0700

    complete config to connect MongoDB Database

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,1 +1,32 @@
+import 'dotenv/config';
+import {MongoClient, ServerApiVersion} from 'mongodb';
 
+let mExpensesDatabaseInstance = null;
+
+const mongoClientInstance = new MongoClient(process.env.MONGODB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+export const CONNECT_DB = async () => {
+  await mongoClientInstance.connect();
+
+  mExpensesDatabaseInstance = mongoClientInstance.db(process.env.DATABASE_NAME);
+};
+
+/**
+ * This function GET_DB (not Async function), it exports Database Instance after connected to MongoDB successfully
+ * to use it at anywhere.
+ * Note: Make sure this function calls only after it connected to MongoDB successfully.
+ * @returns mExpensesDatabaseInstance
+ */
+export const GET_DB = () => {
+  if (!mExpensesDatabaseInstance) {
+    throw new Error('Must connect to Database first.');
+  }
+
+  return mExpensesDatabaseInstance;
+};
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
@@ -5,29 +6,50 @@ import helmet from 'helmet';
 import morgan from 'morgan';
 // config
 import corsOptions from '~/config/cors.config';
-import accessLogStream from './config/accessLog.config';
+import accessLogStream from '~/config/accessLog.config';
+import {CONNECT_DB, GET_DB} from '~/config/db.config';
 // routes
 import Router from './routes';
 
 const isProduction = process.env.NODE_ENV === 'production';
-const app = express();
 
-app.use(express.urlencoded({extended: true}));
-app.use(express.json());
-// Using Cors to allow some specific origins (other domains) request to server.
-app.use(cors(corsOptions));
-// Using helmet to security for app
-app.use(helmet());
-// Using Morgan to log all requests
-app.use(
-  isProduction ? morgan('combined', {stream: accessLogStream}) : morgan('dev'),
-);
+function START_SERVER() {
+  const app = express();
 
-// Router init
-Router(app);
-
-app.listen(process.env.APP_PORT, process.env.APP_HOSTNAME, () => {
-  console.log(
-    `Server is running at http://${process.env.APP_HOSTNAME}:${process.env.APP_PORT}`,
+  app.use(express.urlencoded({extended: true}));
+  app.use(express.json());
+  // Using Cors to allow some specific origins (other domains) request to server.
+  app.use(cors(corsOptions));
+  // Using helmet to security for app
+  app.use(helmet());
+  // Using Morgan to log all requests
+  app.use(
+    isProduction
+      ? morgan('combined', {stream: accessLogStream})
+      : morgan('dev'),
   );
-});
+
+  // Router init
+  Router(app);
+
+  app.listen(process.env.APP_PORT, process.env.APP_HOSTNAME, () => {
+    console.log(
+      `Server is running at http://${process.env.APP_HOSTNAME}:${process.env.APP_PORT}`,
+    );
+  });
+}
+
+/**
+ * Note: Only when it connects to the Database successfully does it start-sever.
+ */
+(async () => {
+  try {
+    await CONNECT_DB();
+    console.log('Connected to MongoDB Cloud Atlas!');
+
+    START_SERVER();
+  } catch (error) {
+    console.error(error);
+    process.exit(0);
+  }
+})();
