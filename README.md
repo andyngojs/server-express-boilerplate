0895ba9f9deba832e3c458aa298cb8e45ff7b8b2
dongngo <dongnbas@gmail.com>
2024-05-28 14:33:16 +0700

    chore: refactor to boilerplate

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-## Server M-Expense
+## Server Express Boilerplate
+
+This is a boilerplate for backend expressJs project.
+
+Default: Connect to MongoDB Atlas Cloud. config connect to db in `db.config.js` file and edit it according your project.
+
--- src/routes/api/v1/users.route.js
+++ src/routes/api/v1/users.route.js
@@ -1,9 +1,8 @@
 import {Router} from 'express';
-import {signInController, signUpController} from '~/controllers';
+import {signInController} from '~/controllers';
 
 const userRouter = Router();
 
 userRouter.get('/sign-in', signInController);
-userRouter.post('/sign-up', signUpController);
 
 export default userRouter;
